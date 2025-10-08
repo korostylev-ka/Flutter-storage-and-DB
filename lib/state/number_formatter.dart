8d9821e5fd688827065ef67fe412964fb679af14
k.korostylev <k.korostylev@gmail.com>
2025-10-08 07:25:24 +0500

    Sql init commit

---
+++ lib/state/number_formatter.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart';
+
+class NumberFormatter extends TextInputFormatter {
+  
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    // TODO: implement formatEditUpdate
+    throw UnimplementedError();
+  }
+
+}
