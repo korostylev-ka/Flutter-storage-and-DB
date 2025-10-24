464bfaa100adf5ce2cb719c5210f9a86ba61986a
k.korostylev <k.korostylev@gmail.com>
2025-10-24 15:42:42 +0500

    Router

--- lib/components/state_app_demo_widget.dart
+++ lib/components/state_app_demo_widget.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_storage/components/add_person_widget.dart';
+import 'package:flutter_storage/components/edit_person_widget.dart';
+import 'package:flutter_storage/components/persons_widget.dart';
+import 'package:flutter_storage/state/contact_book_state.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class StateAppDemoWidget extends StatelessWidget {
   const StateAppDemoWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ContactBookState contactBookState = ContactBookState();
+    contactBookState.load();
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => contactBookState)
+    ],
+    child: MaterialApp.router(
+      title: 'SQL Demo',
+      routerConfig: router(),
+    )
+    );
+  }
+
+  GoRouter router() {
+    return GoRouter(
+      initialLocation: '/',
+        routes: [
+          GoRoute(
+              path: '/',
+            builder: (context, state) => const PersonsWidget(),
+            routes: [
+              GoRoute(
+                  path: 'add',
+                builder: (context, state) => const AddPersonWidget()
+              ),
+              GoRoute(
+                  path: 'edit/:id',
+                builder: (context, state) => EditPersonWidget(id: state.pathParameters['id']!)
+              )
+            ]
+          )
+        ]);
   }
 }
